11f9e563f2895943761655453e03d94c7480c20b
Gaurav Kumar <princegauravaj@gmail.com>
2026-02-13 10:57:24 +0000

    i want this type of thin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           crossOrigin=""
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
